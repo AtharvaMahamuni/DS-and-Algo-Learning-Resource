2df41700e20e27de909c94696244d42b3dcb43bc
Jiganesh Patil <67581447+Jiganesh@users.noreply.github.com>
2020-07-02 14:14:00 +0530

    Add files via upload

---
+++ 01_Recursion/03_tower_of_hanoi.py
@@ -0,0 +1,36 @@
+'''
+Contributed by Jiganesh Patil
+
+Hi there, I bet If you are seeing this you may have heard about Famous Puzzle "Tower of Hanoi"
+
+This is Recursive Solution to Solve Problem of Hanoi
+
+'''
+
+def hanoi(n,source,target,spare):
+
+          global count
+          if n>0:
+
+                    # move n-1 disks from source to spare so that they are out of the way
+                    hanoi(n-1,source,spare,target)
+
+                    
+                    # move the nth disk from source to target
+                    target.append(source.pop())
+                    count+=1
+                    
+                    # move n-1 disks Spare to target
+                    hanoi(n-1,spare,target,source)
+          return count
+
+
+x=int(input("Enter the Number of discs"))
+count=0
+A=list(range(x,0,-1))
+B=[]
+C=[]
+count=hanoi(x,A,C,B)
+print(count)
+
+                    
