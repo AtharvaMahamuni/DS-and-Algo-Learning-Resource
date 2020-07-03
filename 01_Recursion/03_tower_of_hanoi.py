e9528cd1e9c9cec8490af4d86c012ee04e79466f
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-03 13:25:15 +0530

    update

--- 01_Recursion/03_tower_of_hanoi.py
+++ 01_Recursion/03_tower_of_hanoi.py
@@ -7,30 +7,28 @@ This is Recursive Solution to Solve Problem of Hanoi
 
 '''
 
-def hanoi(n,source,target,spare):
 
-          global count
-          if n>0:
+def hanoi(n, source, target, spare):
 
-                    # move n-1 disks from source to spare so that they are out of the way
-                    hanoi(n-1,source,spare,target)
+    global count
+    if n > 0:
 
-                    
-                    # move the nth disk from source to target
-                    target.append(source.pop())
-                    count+=1
-                    
-                    # move n-1 disks Spare to target
-                    hanoi(n-1,spare,target,source)
-          return count
+        # move n-1 disks from source to spare so that they are out of the way
+        hanoi(n-1, source, spare, target)
+
+        # move the nth disk from source to target
+        target.append(source.pop())
+        count += 1
+
+        # move n-1 disks Spare to target
+        hanoi(n-1, spare, target, source)
+    return count
 
 
-x=int(input("Enter the Number of discs"))
-count=0
-A=list(range(x,0,-1))
-B=[]
-C=[]
-count=hanoi(x,A,C,B)
+x = int(input("Enter the Number of discs"))
+count = 0
+A = list(range(x, 0, -1))
+B = []
+C = []
+count = hanoi(x, A, C, B)
 print(count)
-
-                    
